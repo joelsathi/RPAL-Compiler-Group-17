ea9f49f6c8561f474ff46c48f97b0086320e904c
Radith Samarakoon <jjeli1234@gmail.com>
2023-06-14 11:09:26 +0530

    grammar + AST + ST

--- AST.h
+++ AST.h
@@ -8,41 +8,123 @@
 
 using namespace std;
 
-struct Node{
+struct Node
+{
     string data;
-    vector<Node*> children;
+    string type = "internal node";
+    vector<Node *> children;
 };
 
-class Graph{
-    public:
-        Graph(){
-            this->root = new Node();
+class Graph
+{
+public:
+    Graph()
+    {
+        this->root = new Node();
+    }
+
+    Node *get_root()
+    {
+        return this->root;
+    }
+
+    void build_tree(string data, int pop_tree_cnt)
+    {
+        if (this->node_stack.size() == 0)
+        {
+            this->root->data = data;
+            this->node_stack.push_back(this->root);
+            return;
+        }
+        if (pop_tree_cnt == 0)
+        {
+            this->push_to_stack(data);
+            return;
+        }
+        Node *node = new Node();
+        node->data = data;
+
+        for (int i = 0; i < pop_tree_cnt; i++)
+        {
+            Node *curr = this->node_stack.back();
+            this->node_stack.pop_back();
+            node->children.push_back(curr);
         }
 
-        Node* get_root(){
-            return this->root;
+        reverse(node->children.begin(), node->children.end());
+
+        this->node_stack.push_back(this->root);
+    }
+
+    void push_to_stack(string node_name, string node_type = "internal node")
+    {
+        Node *node = new Node();
+        node->data = node_name;
+        node->type = node_type;
+        this->node_stack.push_back(node);
+    }
+
+    void print_node_stack()
+    {
+        for (int i = 0; i < this->node_stack.size(); i++)
+        {
+            std::cout << this->node_stack[i]->data << endl;
         }
+    }
 
-        void build_tree(string data, int pop_tree_cnt){
+    void print_graph()
+    {
+        std::cout << "==================================" << endl;
+        std::cout << "Printing the graph" << endl;
+        std::cout << "==================================" << endl;
 
+        // std::cout << "Root -> " << this->root.data << endl;
+
+        std::cout << "\n\n"
+                  << endl;
+
+        // do BFS
+        vector<Node *> queue;
+        queue.push_back(this->root);
+
+        while (queue.size() > 0)
+        {
+            Node *cur_node = queue[0];
+            queue.erase(queue.begin());
+            std::cout << "Parent Node -> " << cur_node->data << endl;
+            std::cout << "Children -> " << endl;
+            for (int i = 0; i < cur_node->children.size(); i++)
+            {
+                queue.push_back(cur_node->children[i]);
+                std::cout << "Child Node -> " << cur_node->children[i]->data << endl;
+            }
+            std::cout << "\n\n"
+                      << endl;
         }
+    }
 
-        void push_to_stack(string node_data){
+private:
+    vector<string> graph;
+    Node *root;
+    vector<Node *> node_stack;
 
+    void add_node(Node *node, Node *parent)
+    {
+        parent->children.push_back(node);
+    }
+
+    void inorder(Node *node)
+    {
+        if (node->children.size() == 0)
+        {
+            std::cout << node->data << endl;
+            return;
         }
-
-        void print_AST(){
-
+        for (int i = 0; i < node->children.size(); i++)
+        {
+            inorder(node->children[i]);
         }
-
-    private:
-        vector<string> graph;
-        Node* root;
-        vector<Node*> node_stack;
-
-        void add_node(Node* node, Node* parent){
-            
-        }
+    }
 };
 
 #endif
--- ST.h
+++ ST.h
@@ -9,77 +9,508 @@
 
 using namespace std;
 
-class ST{
-    public:
-        ST(Node* root){
-            this->st_root = root;
+class ST
+{
+public:
+    ST(Node *root)
+    {
+        this->st_root = root;
+    }
+
+    /**
+     * @brief This will construct the standard tree.
+     *
+     */
+    void standardize()
+    {
+        construct_ST_rec(this->st_root);
+    }
+
+    /**
+     * @brief This will print the standard tree.
+     *
+     */
+    void print_ST()
+    {
+        std::cout << "==================================" << endl;
+        std::cout << "Printing the Standard Tree ( BFS )" << endl;
+        std::cout << "==================================" << endl;
+
+        std::cout << "\n\n"
+                  << endl;
+
+        vector<Node *> queue;
+        queue.push_back(this->st_root);
+
+        while (queue.size() > 0)
+        {
+            Node *cur_node = queue[0];
+            queue.erase(queue.begin());
+            std::cout << "Parent Node -> " << cur_node->data << endl;
+            std::cout << "Children -> " << endl;
+            for (int i = 0; i < cur_node->children.size(); i++)
+            {
+                queue.push_back(cur_node->children[i]);
+                std::cout << "Child Node -> " << cur_node->children[i]->data << endl;
+            }
+            std::cout << "\n\n"
+                      << endl;
+        }
+    }
+
+private:
+    Node *st_root;
+
+    /**
+     * @brief This will construct the standard tree recursively.
+     *
+     * @param node
+     */
+    void construct_ST_rec(Node *node)
+    {
+        for (int i = 0; i < node->children.size(); i++)
+        {
+            construct_ST_rec(node->children[i]);
         }
 
-        /**
-         * @brief This will construct the standard tree.
-         * 
-         */
-        void standardize(){
+        stardardize_node(node);
+    }
 
+    /**
+     * @brief This will check the data of the node and standardize by
+     * calling the appropriate function.
+     *
+     * @param node
+     */
+    void stardardize_node(Node *node)
+    {
+        if (node->data.compare("let") == 0)
+        {
+            standardize_let(node);
+        }
+        else if (node->data.compare("fcn_form") == 0)
+        {
+            standadize_fcn_form(node);
         }
 
-        /**
-         * @brief This will print the standard tree.
-         * 
-         */
-        void print_ST(){
-
+        else if (node->data.compare("where") == 0)
+        {
+            standardize_where(node);
         }
-    
-    private:
-        Node* st_root;
+        else if (node->data.compare("lambda") == 0 && node->children[0]->data.compare(",") != 0)
+        {
+            standardize_lambda(node);
+        }
+        else if (node->data.compare("within") == 0)
+        {
+            standardize_within(node);
+        }
+        else if (node->data.compare("@") == 0)
+        {
+            standardize_at(node);
+        }
+        else if (node->data.compare("and") == 0)
+        {
+            standardize_and(node);
+        }
+        else if (node->data.compare("rec") == 0)
+        {
+            standardize_rec(node);
+        }
+        else
+        {
+            stardardize_node(node);
+        }
+    }
 
-        /**
-         * @brief This will construct the standard tree recursively.
-         * 
-         * @param node 
-         */
-        void construct_ST_rec(Node* node){
+    bool checkType(string token, string str_type)
+    {
 
+        vector<string> keywords = {"let", "in", "fn", "where", "aug",
+                                   "or", "not", "gr", "ge", "ls", "le", "eq", "ne",
+                                   "true", "false", "nil", "dummy", "within", "and",
+                                   "rec", "Isinteger"};
+
+        if (str_type.compare("identifier") == 0)
+        {
+            // We don't want to match a keyword as an identifier
+            if (std::find(keywords.begin(), keywords.end(), token) != keywords.end())
+            {
+                return false;
+            }
+            regex identifier_pat("[a-zA-Z][a-zA-Z0-9_]*");
+            return regex_match(token, identifier_pat);
+        }
+        else if (str_type.compare("integer") == 0)
+        {
+            regex integer_pat("[0-9]+");
+            return regex_match(token, integer_pat);
+        }
+        else if (str_type.compare("string") == 0)
+        {
+            regex string_pat("\'[^\"]*\'");
+            return regex_match(token, string_pat);
+        }
+        return false;
+    }
+
+    void standardize_let(Node *node)
+    {
+        Node *child_0 = node->children[0];
+        Node *child_1 = node->children[1];
+
+        Node *eq;
+        Node *p;
+        Node *e;
+
+        if (child_0->data.compare("=") == 0)
+        {
+            eq = child_0;
+            p = child_1;
+        }
+        else if (child_1->data.compare("=") == 0)
+        {
+            eq = child_1;
+            p = child_0;
+        }
+        else
+        {
+            throw std::invalid_argument("Invalid let expression");
         }
 
-        /**
-         * @brief This will check the data of the node and standardize by
-         * calling the appropriate function.
-         * 
-         * @param node 
-         */
-        void stardardize_node(Node* node){
+        Node *x;
+        Node *e;
 
+        if (checkType(eq->children[0]->data, "identifier"))
+        {
+            x = eq->children[0];
+            e = eq->children[1];
+        }
+        else if (checkType(eq->children[1]->data, "identifier"))
+        {
+            x = eq->children[1];
+            e = eq->children[0];
+        }
+        else
+        {
+            throw std::invalid_argument("Invalid syntax: Expected an identifier");
         }
 
-        void standardize_let(Node* node){
+        node->data = "gamma";
+        node->children.clear();
 
+        eq->data = "lambda";
+        eq->children.clear();
+
+        eq->children.push_back(x);
+        eq->children.push_back(p);
+
+        node->children.push_back(eq);
+        node->children.push_back(e);
+    }
+
+    void standardize_where(Node *node)
+    {
+        Node *child_0 = node->children[0];
+        Node *child_1 = node->children[1];
+
+        Node *eq;
+        Node *p;
+        Node *e;
+
+        if (child_0->data.compare("=") == 0)
+        {
+            eq = child_0;
+            p = child_1;
+        }
+        else if (child_1->data.compare("=") == 0)
+        {
+            eq = child_1;
+            p = child_0;
+        }
+        else
+        {
+            throw std::invalid_argument("Invalid where expression");
         }
 
-        void standardize_where(Node* node){
+        Node *x;
+        Node *e;
 
+        if (checkType(eq->children[0]->data, "identifier"))
+        {
+            x = eq->children[0];
+            e = eq->children[1];
+        }
+        else if (checkType(eq->children[1]->data, "identifier"))
+        {
+            x = eq->children[1];
+            e = eq->children[0];
+        }
+        else
+        {
+            throw std::invalid_argument("Invalid syntax: Expected an identifier");
         }
 
-        void standadize_fcn_form(Node* node){
+        node->data = "gamma";
+        node->children.clear();
 
+        eq->data = "lambda";
+        eq->children.clear();
+
+        eq->children.push_back(x);
+        eq->children.push_back(p);
+
+        node->children.push_back(eq);
+        node->children.push_back(e);
+    }
+
+    void standadize_fcn_form(Node *node)
+    {
+        Node *p = node->children[0];
+
+        vector<Node *> vp_nodes;
+
+        for (int i = 1; i < node->children.size() - 1; i++)
+        {
+            Node *curr = node->children[i];
+            vp_nodes.push_back(curr);
         }
 
-        void standardize_lambda(Node* node){
+        Node *e = node->children[node->children.size() - 1];
 
+        node->children.clear();
+        node->data = "=";
+        node->children.push_back(p);
+
+        for (int i = 0; i < vp_nodes.size(); i++)
+        {
+            Node *curr_vp = vp_nodes[i];
+            Node *lambda = new Node();
+            lambda->data = "lambda";
+            lambda->children.push_back(curr_vp);
+
+            node->children.push_back(lambda);
+
+            node = lambda;
         }
 
-        void standardize_within(Node* node){
+        node->children.push_back(e);
+    }
 
+    void standardize_lambda(Node *node)
+    {
+        vector<Node *> vp_nodes;
+
+        while (checkType(node->children[0]->data, "identifier"))
+        {
+            vp_nodes.push_back(node->children[0]);
         }
 
-        void standardize_at(Node* node){
+        Node *e = node->children[node->children.size() - 1];
 
+        node->children.clear();
+        node->children.push_back(vp_nodes[0]);
+
+        // If one variable lambda, then we don't need to do anything
+        for (int i = 1; i < vp_nodes.size(); i++)
+        {
+            Node *lambda = new Node();
+            lambda->data = "lambda";
+            lambda->children.push_back(vp_nodes[i]);
+            node->children.push_back(lambda);
+            node = lambda;
         }
 
-        void standardize_and(Node* node){
+        node->children.push_back(e);
+    }
 
+    void standardize_within(Node *node)
+    {
+        Node *eq1 = node->children[0];
+        Node *eq2 = node->children[1];
+
+        if (eq1->data.compare("=") != 0 || eq2->data.compare("=") != 0)
+        {
+            throw std::invalid_argument("Invalid within expression");
         }
+
+        Node *x1;
+        Node *e1;
+
+        if (checkType(eq1->children[0]->data, "identifier"))
+        {
+            x1 = eq1->children[0];
+            e1 = eq1->children[1];
+        }
+        else if (checkType(eq1->children[1]->data, "identifier"))
+        {
+            x1 = eq1->children[1];
+            e1 = eq1->children[0];
+        }
+        else
+        {
+            throw std::invalid_argument("Invalid syntax: Expected an identifier");
+        }
+
+        Node *x2;
+        Node *e2;
+
+        if (checkType(eq2->children[0]->data, "identifier"))
+        {
+            x2 = eq2->children[0];
+            e2 = eq2->children[1];
+        }
+        else if (checkType(eq2->children[1]->data, "identifier"))
+        {
+            x2 = eq2->children[1];
+            e2 = eq2->children[0];
+        }
+        else
+        {
+            throw std::invalid_argument("Invalid syntax: Expected an identifier");
+        }
+
+        node->data = "=";
+        node->children.clear();
+
+        eq1->data = "lambda";
+        eq1->children.clear();
+        eq2->data = "gamma";
+        eq2->children.clear();
+
+        eq1->children.push_back(x1);
+        eq1->children.push_back(e2);
+
+        eq2->children.push_back(eq1);
+        eq2->children.push_back(e1);
+
+        node->children.push_back(x2);
+        node->children.push_back(eq2);
+    }
+
+    void standardize_at(Node *node)
+    {
+        Node *e1 = node->children[0];
+        Node *n = node->children[1];
+        Node *e2 = node->children[2];
+
+        node->data = "gamma";
+        node->children.clear();
+
+        Node *gamma2 = new Node();
+        gamma2->data = "gamma";
+
+        node->children.push_back(gamma2);
+        node->children.push_back(e2);
+
+        gamma2->children.push_back(n);
+        gamma2->children.push_back(e1);
+    }
+
+    void standardize_rec(Node *node)
+    {
+        Node *rec_node = node->children[0];
+        Node *eq_node = node->children[1];
+
+        if (eq_node->data.compare("=") != 0)
+        {
+            throw std::invalid_argument("Invalid rec expression");
+        }
+
+        Node *x;
+        Node *e;
+
+        if (checkType(eq_node->children[0]->data, "identifier"))
+        {
+            x = eq_node->children[0];
+            e = eq_node->children[1];
+        }
+        else if (checkType(eq_node->children[1]->data, "identifier"))
+        {
+            x = eq_node->children[1];
+            e = eq_node->children[0];
+        }
+        else
+        {
+            throw std::invalid_argument("Invalid syntax: Expected an identifier");
+        }
+
+        rec_node->data = "=";
+        rec_node->children.clear();
+
+        eq_node->data = "gamma";
+        eq_node->children.clear();
+
+        Node *y_node = new Node();
+        y_node->data = "Y";
+
+        Node *lambda_node = new Node();
+        lambda_node->data = "lambda";
+
+        eq_node->children.push_back(y_node);
+        eq_node->children.push_back(lambda_node);
+
+        lambda_node->children.push_back(x);
+        lambda_node->children.push_back(e);
+
+        rec_node->children.push_back(x);
+        rec_node->children.push_back(eq_node);
+    }
+
+    void standardize_and(Node *node)
+    {
+        vector<Node *> eq_nodes;
+
+        for (int i = 0; i < node->children.size(); i++)
+        {
+            Node *curr = node->children[i];
+            if (curr->data.compare("=") != 0)
+            {
+                throw std::invalid_argument("Invalid and expression");
+            }
+            eq_nodes.push_back(curr);
+        }
+
+        node->data = "=";
+        node->children.clear();
+
+        Node *comma = new Node();
+        comma->data = ",";
+
+        Node *tau = new Node();
+        tau->data = "tau";
+
+        node->children.push_back(comma);
+        node->children.push_back(tau);
+
+        for (int i = 0; i < eq_nodes.size(); i++)
+        {
+            Node *curr_eq = eq_nodes[i];
+            Node *x;
+            Node *e;
+
+            if (checkType(curr_eq->children[0]->data, "identifier"))
+            {
+                x = curr_eq->children[0];
+                e = curr_eq->children[1];
+            }
+            else if (checkType(curr_eq->children[1]->data, "identifier"))
+            {
+                x = curr_eq->children[1];
+                e = curr_eq->children[0];
+            }
+            else
+            {
+                throw std::invalid_argument("Invalid syntax: Expected an identifier");
+            }
+
+            comma->children.push_back(x);
+
+            tau->children.push_back(e);
+
+            delete curr_eq;
+        }
+    }
 };
 
 #endif
