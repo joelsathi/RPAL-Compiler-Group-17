365791eeedb4dd35d8f10de697c9442f78250bee
Ama-Pathirana <119398010+Ama-Pathirana@users.noreply.github.com>
2023-07-19 17:52:26 +0530

    Update grammer.h

--- grammer.h
+++ grammer.h
@@ -128,8 +128,11 @@ private:
             }
             // std::cout << "Consuming " << this->token << " in E()" << endl;
             consume(".");
-            else throw std::runtime_error("Can't parse the given input! \'.\' is missing");
-            // std::cout << "Calling Tc from Tc with cur Token -> " << this->token << endl;
+            else 
+            {    
+            throw std::runtime_error("Can't parse the given input! \'.\' is missing");
+            }
+            // std::cout << "Calling E from E with cur Token -> " << this->token << endl;
 
             E();
 
