2d67e2cded3720affe0f29a4063a1e5ec95393fe
Ama-Pathirana <119398010+Ama-Pathirana@users.noreply.github.com>
2023-07-24 17:09:30 +0530

    Update tokenizer.h

--- tokenizer.h
+++ tokenizer.h
@@ -31,9 +31,37 @@ class Tokenizer{
         };
 
         // Ama
-        void lexer(){
-            // Go through the input string and tokenize it
+        void lexer()
+{
+        // Go through the input string and tokenize it
+        string currentToken = "";
+        for (char ch : input)
+        {
+        // Check if the current character is a whitespace
+            if (isspace(ch))
+            {
+                // If the current token is not empty, add it to the list of tokens
+                if (!currentToken.empty())
+                {
+                    tokens.push_back(currentToken);
+                    currentToken = ""; // Reset the temporary storage
+                }
+                // Add the operator symbol as a separate token
+                tokens.push_back(string(1, ch));
+            }
+            else 
+            {
+            // If the current character is not a whitespace or operator symbol, add it to the current token
+            currentToken += ch;
+            }
+            
+         }
+         // Add the last token (if any) after the loop finishes
+        if (!currentToken.empty())
+        {
+            tokens.push_back(currentToken);
         }
+    }
 };
 
 #endif
